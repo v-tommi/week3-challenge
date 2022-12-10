33e61d8e2298260100ed33841bcd699758ca2345
v-tommi <tom.miyahira@outlook.com>
2022-12-09 15:18:55 -1000

    created prompt/confirms for req questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,6 +9,33 @@ function writePassword() {
   var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numeric = "0123456789";
   var specialchar = "!@#$%^&*()";
+  
+  let pwlength = prompt(
+    "Choose password length." + "\n" +
+    "(Minimum character requirements: 8-128)"
+    );
+  
+  let pwrequpper = confirm(
+    "Require UPPER case characters?" + "\n" +
+    "(Ok = Yes, Cancel = No)"
+  );
+  
+  let pwreqlower = confirm(
+    "Require lower case characters?" + "\n" +
+    "(Ok = Yes, Cancel = No)"
+  );
+
+  let pwreqnumbers = confirm(
+    "Require numeric values?" + "\n" +
+    "(Ok = Yes, Cancel = No)"
+  );
+
+  let pwreqspecial = confirm(
+    "Require special characters?" + "\n" +
+    "(Ok = Yes, Cancel = No)"
+  );
+
+  Math.floor(Math.random(pwlength))
 
   passwordText.value = password;
 
